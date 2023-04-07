917288bd9662c33b603728dd529aab85697127ed
SJW <96479626+MildColor@users.noreply.github.com>
2023-04-07 14:07:42 +0900

    Fix header 로그아웃 버튼 조건 수정

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -4,3 +4,10 @@ export const Title = styled.h1`
   font-size: 40px;
   color: white;
 `;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { PAGE_PATH } from "../../constants/path";
-import { removeLocalStorage } from "../../utils/localStorage";
+import { getLocalStorage, removeLocalStorage } from "../../utils/localStorage";
 import * as Style from "./Header.styles";
+import Button from "../common/Button/Button";
 
 function Header({ title }: { title: string }) {
   const navigate = useNavigate();
+
+  const token = getLocalStorage("accessToken");
 
   const logoutHandler = () => {
     removeLocalStorage("accessToken");
@@ -14,10 +17,16 @@ function Header({ title }: { title: string }) {
   };
 
   return (
-    <>
+    <Style.Container>
       <Style.Title>{title}</Style.Title>
-      {/* <button onClick={logoutHandler}>로그아웃</button> */}
-    </>
+      {token ? (
+        <Button onClick={logoutHandler} width="50px">
+          로그아웃
+        </Button>
+      ) : (
+        <></>
+      )}
+    </Style.Container>
   );
 }
 
