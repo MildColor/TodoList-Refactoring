b2b172eb960a598dae1e8518554a4da3c8cef3c0
SJW <96479626+MildColor@users.noreply.github.com>
2023-04-15 16:58:40 +0900

    Update TodoDetail CSS

--- src/pages/TodoDetail.tsx
+++ src/pages/TodoDetail.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { useGetTodoById } from "../hooks/queries/todo/useGetTodoById";
 import { flexColumn } from "../styles/mixins";
 
 function TodoDetail() {
   const { id } = useParams();
-
+  console.log(id);
   const { data: getTodoByIdData } = useGetTodoById(id);
   const { title, content, createdAt, updatedAt } = getTodoByIdData;
 
@@ -14,14 +14,8 @@ function TodoDetail() {
     <DetailContainer>
       <h1>{title}</h1>
       <p>{content}</p>
-      <p>
-        createdAt:
-        {createdAt.slice(0, 10) + " " + createdAt.slice(11, 19)}
-      </p>
-      <p>
-        updatedAt:
-        {updatedAt.slice(0, 10) + " " + updatedAt.slice(11, 19)}
-      </p>
+      <p>createdAt: {createdAt.slice(0, 10) + " " + createdAt.slice(11, 19)}</p>
+      <p>updatedAt: {updatedAt.slice(0, 10) + " " + updatedAt.slice(11, 19)}</p>
     </DetailContainer>
   );
 }
@@ -30,23 +24,27 @@ export default TodoDetail;
 
 const DetailContainer = styled.div`
   ${flexColumn}
+  justify-content: space-evenly;
   width: 540px;
   height: 100%;
-  padding: 15px;
+  padding: 20px;
 
   h1 {
     font-size: 20px;
     font-weight: bold;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
   }
   p {
-    margin-bottom: 20px;
+    margin-bottom: 10px;
+  }
+
+  p:nth-child(2) {
+    height: 460px;
   }
 
   * {
     overflow: hidden;
     text-overflow: ellipsis;
-    white-space: nowrap;
     word-break: break-all;
   }
 `;
