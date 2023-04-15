5f1b502cb4ed931f6ee668690f0ffe21d42f12b1
SJW <96479626+MildColor@users.noreply.github.com>
2023-04-15 16:53:59 +0900

    Feat VerticalLine Component

---
+++ src/components/common/VerticalLine/VerticalLine.styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Line = styled.div`
+  width: 3px;
+  height: 100%;
+  background-image: ${({ theme }) => theme.gradients.RainyAshville};
+`;
---
+++ src/components/common/VerticalLine/VerticalLine.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as Style from "./VerticalLine.styles";
+
+function VerticalLine() {
+  return <Style.Line />;
+}
+
+export default VerticalLine;
